0eabde77308620cc4860097124aaa8536e0a4738
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-10-28 06:34:42 +0200

    .

--- M3-D3.js
+++ M3-D3.js
@@ -30,7 +30,7 @@ const loadImages = () => {
 
         col.innerHTML = `
         <div class="card mb-4 shadow-sm">
-<img src="${photo.src.medium}" class="img-fluid squared">
+<img src="${photo.src.medium}" class="img-card">
 <div class="card-body">
   <p class="card-text">
   <title>${photo.photographer}</title>
@@ -82,32 +82,66 @@ const loadSecondaryImages = () => {
       const row = document.querySelector(".album .row");
 
       console.log(body.photos.length);
-      console.log(Boolean(0 < body.photos.length));
 
       row.innerHTML = "";
 
       for (let i = 0; i < body.photos.length; i++) {
         const photo = body.photos[i];
-
         // for (const photo of body.photos) {
 
         const col = document.createElement("div");
         col.className = "col-md-4";
 
         col.innerHTML = `
-            
-            <div class="card">
-                <img src="${photo.src.medium}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${photo.photographer}</h5>
-                    <p class="card-text">${photo.photographer_id}€</p>
-                </div>
-            </div>
-            `;
+        <div class="card mb-4 shadow-sm">
+<img src="${photo.src.medium}" class="img-card">
+<div class="card-body">
+  <p class="card-text">
+  <title>${photo.photographer}</title>
+
+  </p>
+  <div
+    class="d-flex justify-content-between align-items-center"
+  >
+    <div class="btn-group">
+      <button
+        type="button"
+        class="btn btn-sm btn-outline-secondary"
+      >
+        View
+      </button>
+      <button
+        type="button"
+        class="btn btn-sm btn-outline-secondary" onclick="buttonHide()"
+      >
+        Edit
+      </button>
+    </div>
+    <small class="text-muted">${photo.photographer_id}</small>
+  </div>
+</div>
+</div>
+</div>`;
 
         row.appendChild(col);
       }
-    })
+
+      // 3. Replace the **Edit** buttons on the cards with a **Hide** button.
+
+      const buttonValue = document.querySelectorAll(".btn-group:last-child");
+      for (let i = 0; i < buttonValue.length; i++) {
+        buttonValue.innerHTML = "Hide";
+      }
+
+      //  4. When pressed, this button should make the whole card disappear.
+      const buttonHide = function () {
+        const hide = document.querySelectorAll(".row .col-md-4 .card");
+        for (let i = 0; i < body.photos.length - 1; i++) {
+          hide.innerHTML = "";
+        }
+      };
+      // 5. Replace the **9 mins** string in the cards with the ID of the image
+    }) //end of .then
     .catch((error) => {
       console.log(error);
     });
